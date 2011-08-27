e76ee7ab6a0445049020b12e77107a02a95d319d
usuario <usuario@usuario-PC>
2011-08-26 21:07:45 -0500

    loadtexture() function added on gamecore

--- ModelingProject1/SourceCode/GameCore.cpp
+++ ModelingProject1/SourceCode/GameCore.cpp
@@ -1,5 +1,6 @@
 #include "GameCore.h"
-
+#include <string>
+#include "SDL_image.h"
 
 GameCore::GameCore(void)
 {
@@ -25,4 +26,32 @@ GameCore::~GameCore(void)
 	delete saves;
 	delete screen;
 	delete timer;
+}
+
+GLuint loadTexture(std::string name){
+	
+	SDL_Surface* image = load_image(name);
+	
+	GLuint texture;
+
+	if(image!=NULL){
+		
+		glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+		glTexImage2D(	GL_TEXTURE_2D,
+						0, 
+						4, 
+						image->w, 
+						image->h,
+						0, 
+						GL_BGRA,
+						GL_UNSIGNED_BYTE, 
+						image->pixels);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+		SDL_FreeSurface(image);
+		return texture;
+	}
 }
